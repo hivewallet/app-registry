effc8e25fdd721af9d37a0e4e877bfbc7ee03c40
Wei Lu <luwei.here@gmail.com>
2014-02-04 20:31:36 +0800

    manifest server

--- index.js
+++ index.js
@@ -1,133 +1,47 @@
-var git = require('git-node')
-var basename = require('path').basename
-var semver = require('semver-loose')
+var http = require('http')
+var Manifests = require('./manifests.js')
+var EventEmitter = require('events').EventEmitter
 
-function fetch(url, callback) {
-  var remote = git.remote(url);
-  var path = 'repos/' + basename(remote.pathname);
-  var repo = git.repo(path);
+var manifests = ''
 
-  console.log("Cloning %s to %s", url, path);
-
-  var opts = {
-    onProgress: function (progress) {
-      process.stderr.write(progress);
-    },
-    includeTag: true
-  };
-
-  repo.fetch(remote, opts, function(err){
-    console.log("Done: ", path);
-    callback(err, repo)
-  });
-}
-
-function listApps(repo, callback) {
-  // find App Registry
-  repo.treeWalk("HEAD", function(err, tree){
-    tree.read(function(err, entry){
-      var registry = entry.body.filter(function(node){
-        return node.name === 'App-Registry.md'
-      })[0]
-      repo.loadAs("blob", registry.hash, function (err, blob) {
-        if (err) throw err;
-
-        //match links in list
-        var links = [];
-        var regexp = /^[-|*|+]\s+\[(.*)\]\((.*)\)/gm;
-        var match = regexp.exec(blob.toString())
-        while (match != null) {
-          links.push(match[2])
-          match = regexp.exec(blob.toString())
-        }
-
-        var fetchRemaining = links.length
-        var manifests = []
-        links.forEach(function(repoUrl){
-          fetch(repoUrl, function(err, repo) {
-            if(err)  {
-              // ignore error
-              console.error("fetch error: ", repoUrl, err.stack)
-              if(!--fetchRemaining) { callback() }
-              return
-            }
-
-            getTags(repo, function(err, manifest){
-              // ignore error
-              if(err) console.error("getTags error: ", repoUrl, err.stack);
-
-              if(manifest) manifests.push(manifest)
-              if(!--fetchRemaining) { callback(null, manifests) }
-            })
-          })
-        })
-      })
-    })
-  })
-}
-
-function getTags(repo, callback) {
-  repo.listRefs("refs/tags", function(err, refs){
-    if(err) return callback(err);
-
-    var versions = Object.keys(refs)
-    if(versions.length === 0) return callback();
-
-    var latestTag = versions.sort(semver.sort).reverse()[0]
-    getManifest(repo, refs[latestTag], callback)
-  })
-}
-
-function getManifest(repo, sha, callback) {
-  search(repo, sha, "manifest.json", callback)
-}
-
-function search(repo, sha, filename, callback) {
-  repo.treeWalk(sha, function(err, tree){
-    if(err) return callback(err);
-
-    tree.read(function(err, entry){
-      if(err) return callback(err);
-
-      var file = entry.body.filter(function(n){
-        return n.name === filename
-      })[0]
-
-      if(file === undefined) {
-        entry.body.filter(function(node){
-          return node.path.match(/\/$/)
-        }).forEach(function(n){
-          search(repo, n.hash, filename, callback)
-        })
-      } else {
-        repo.loadAs("blob", file.hash, function (err, blob) {
-          if(err) return callback(err)
-
-          toJSON(blob.toString(), function(err, data) {
-            callback(err, data)
-            callback = function(){ console.warn("calling load callback multiple times!") }
-          })
-        })
-      }
-    })
-  })
-}
-
-function toJSON(data, callback){
-  try{
-    callback(null, JSON.parse(data))
-  } catch (e) {
-    callback(e)
+var server = http.createServer(function(req, res) {
+  if (!req.url.match(/index\.json$/)) {
+    res.statusCode = 404
+    return res.end()
   }
-}
 
-fetch("https://github.com/hivewallet/hive-osx.wiki.git", function(err, repo){
-  if(err) { throw err }
+  res.setHeader('Content-Type', 'text/json')
 
-  listApps(repo, function(err, manifests){
-    if(err) { throw err }
+  if (!manifests) return loadManifests(res)
 
-    console.log(JSON.stringify(manifests))
-  })
+  writeResponse(res)
 })
 
+var manifestReady = new EventEmitter()
+
+function loadManifests(res) {
+  if (loadManifests.isLoading) {
+    return manifestReady.once('ready', function() {
+      writeResponse(res)
+    })
+  }
+
+  loadManifests.isLoading = true
+
+  Manifests(function(err, m) {
+    if (err) return console.error('manifest error: %s \n', err.message, err.stack)
+
+    manifests = m
+    loadManifests.isLoading = false
+    manifestReady.emit('ready')
+
+    writeResponse(res)
+  })
+}
+
+function writeResponse(res) {
+  res.write(JSON.stringify(manifests))
+  res.end()
+}
+
+server.listen(9009)
